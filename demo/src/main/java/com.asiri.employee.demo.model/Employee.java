dd5db5c64fe5d911339539d0a2db8b82fd7570e3
AsiriRajapaksha <asiri.l.rajapaksha59@gmail.com>
2020-04-02 23:28:16 +0530

    first commit

---
+++ demo/src/main/java/com.asiri.employee.demo.model/Employee.java
@@ -0,0 +1,40 @@
+public class Employee{
+
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getfirstName() {
+        return fName;
+    }
+
+    public void setfirstName(String firstName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
